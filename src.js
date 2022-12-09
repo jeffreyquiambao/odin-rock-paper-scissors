63a39d4a8e79b42a83862e8d02f7e45be5cdeaee
Jeffrey Quiambao <jcquiamb@uwaterloo.ca>
2022-12-09 16:09:17 -0500

    add play round function

--- src.js
+++ src.js
@@ -12,3 +12,44 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerChoice = playerSelection.toLowerCase();
+    computerChoice = computerSelection.toLowerCase();
+
+    if (playerChoice == "rock") {
+        if (computerChoice == "rock") {
+            return "Tie game!"
+        } else if (computerChoice == "paper") {
+            return "You Lose! Paper beats Rock"
+        } else if (computerChoice == "scissors") {
+            return "You Win! Rock beats Scissors"
+        }  
+    } else if (playerChoice == "paper") {
+        if (computerChoice == "rock") {
+            return "You Win! Paper beats Rock"
+        } else if (computerChoice == "paper") {
+            return "Tie game!"
+        } else if (computerChoice == "scissors") {
+            return "You Lose! Scissors beats Paper"
+        }  
+    } else if (playerChoice == "scissors") {
+        if (computerChoice == "rock") {
+            return "You Lose! Rock beats Scissors"
+        } else if (computerChoice == "paper") {
+            return "You Win! Scissors beats Paper"
+        } else if (computerChoice == "scissors") {
+            return "Tie Game!"
+        }  
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock, paper, or scissors?");
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
